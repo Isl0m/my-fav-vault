809dec9532134caf07ee501b70a4325216502bae
dev_islom <islomrustam09@gmail.com>
2023-07-20 11:15:31 +0500

    small fix

--- src/components/profile-image/image-preview.tsx
+++ src/components/profile-image/image-preview.tsx
@@ -25,9 +25,9 @@ export function UploadedImagePreview({ imagePath, handleChangeImage }: Props) {
       </Button>
     </div>
   ) : (
-    <>
+    <div className='flex flex-col'>
       <Skeleton className='h-24 w-24 rounded-md' />
       <Skeleton className='mt-2 h-10 w-24 rounded-md' />
-    </>
+    </div>
   )
 }
